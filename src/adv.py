6d45b91614ac0b0846d1ed8bc570327a3c769a32
Wei Du <wei.du.2015@marshall.usc.edu>
2019-07-10 18:40:48 -0400

    move working

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,11 @@
 from room import Room
-
+from player import Player
+import textwrap
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -24,28 +25,64 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].n = room['foyer']
+room['foyer'].s = room['outside']
+room['foyer'].n = room['overlook']
+room['foyer'].e = room['narrow']
+room['overlook'].s = room['foyer']
+room['narrow'].w = room['foyer']
+room['narrow'].n = room['treasure']
+room['treasure'].s = room['narrow']
 
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
-
+p = Player('Ray', room['outside'])
 # Write a loop that:
 #
+
+while True:
+    direction = ['n', 's', 'e', 'w', 'q']
 # * Prints the current room name
+    print(p.room.room_name)
+    print(p.room.room_desc)
+    if p.room.room_name == 'Treasure Chamber':
+        break
+    move = input('where do you wanted to go:')
 # * Prints the current description (the textwrap module might be useful here).
+    
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
+    
+    if move == 'n':
+        if p.room.n:
+            p.room = p.room.n
+        else:
+            print('cannot move this direction')
+    elif move == 's':
+        if p.room.s:
+            p.room = p.room.s
+        else:
+            print('cannot move this direction')
+    elif move == 'e':
+        if p.room.e:
+            p.room = p.room.e
+        else:
+            print('cannot move this direction')
+    elif move == 'w':
+        if p.room.w:
+            p.room = p.room.w
+        else:
+            print('cannot move this direction')
+    elif move == 'q':
+        print('goodbye')
+        break
+
+    else:
+        print('you can move e, w, s, n, or enter q to quit')
+    
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, room_name, room_desc):
+        self.room_name = room_name
+        self.room_desc = room_desc
+        self.n = None
+        self.w = None
+        self.s = None
+        self.e = None
+    def __str__(self):
+        return f'{self.room_name}, {self.room_desc}'
