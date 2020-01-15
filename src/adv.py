5bf83c0117c1a7e33c5156da308fdf9c3f3269a7
chef27615 <wei.du.2015@marshall.usc.edu>
2020-01-15 18:45:01 -0500

    moving

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
-
+from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
@@ -35,12 +36,53 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-#
-
+# 
 # Make a new player object that is currently in the 'outside' room.
 
+#room holding item
+r = Item('A Rock')
+room['outside'].items.append(r)
+p = Player(room['outside'])
+print(f'{p.cur_r}\n{p.cur_r.desc}\n{r.name}')
 # Write a loop that:
 #
+while True:
+    move = input('Your move:   ')
+    if move == 'e':
+        if p.cur_r.e_to is None:
+            print('nope')
+        else:
+            p.cur_r = p.cur_r.e_to
+            print(p.cur_r)
+    elif move == 'w':
+        if p.cur_r.w_to is None:
+            print('nope')
+        else:
+            p.cur_r = p.cur_r.w_to
+            print(p.cur_r)
+    elif move == 's':
+        if p.cur_r.s_to is None:
+            print('nope')
+        else:
+            p.cur_r = p.cur_r.s_to
+            print(p.cur_r)
+    elif move == 'n':
+        if p.cur_r.n_to is None:
+            print('nope')
+        else:
+            p.cur_r = p.cur_r.n_to
+            print(p.cur_r)
+    elif move == 'q':
+        break
+    elif move == 'i':
+        p.inv()
+    elif move == 'l':
+        p.look()
+    else:
+        print('enter your move in this format: e,w,n,s,q')
+
+        
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
--- src/player.py
+++ src/player.py
@@ -1,2 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player():
+    def __init__(self, sta_r):
+        self.cur_r = sta_r
+        self.bag = []
+
+    def inv(self):
+        if len(self.bag)>0:
+            for item in self.bag:
+                if item is None:
+                    pass
+                else:
+                    print(item)
+        else:
+            print('nothing in your bag')
+    
+    def look(self):
+        if len(self.cur_r.items)>0:
+            for item in self.cur_r.items:
+                print(item)
+        
